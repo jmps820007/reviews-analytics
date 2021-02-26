1e03aa4669392d805c610fcdb2005d1597f36479
Denny Fan <jmps820007@gmail.com>
2021-02-26 14:39:31 +0800

    add list comprehension

--- read.py
+++ read.py
@@ -24,9 +24,35 @@ print(sum_len)  #data串列的總字元數
 
 print("留言平均長度為", sum_len / count)
 
+##印出字數小於100的留言
 new = []
 for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '比留言長度小於100')
 print(new[0])
+
+##印出提到good的留言
+good = []
+for d in data:
+    if 'good' in d:  #d裡面是否有'good'字串
+        good.append(d)
+print('一共有', len(good), '提到good')
+print(good[0])
+
+####################
+#清單快寫法1
+good = [d for d in data if 'good' in d]  #[|d運算| for |d變數| in |data清單| |if 'good' in d篩選條件|]
+print(good)
+
+####################
+#清單快寫法2
+bad = ['bad' in d for d in data]  #[|d運算| for |d變數| in |data清單|]
+print(bad)
+
+#普通寫法
+bad = []
+for d in data:
+    bad.append('bad' in d)
+
+print(bad)
