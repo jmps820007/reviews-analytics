b2b3f04086fd280716993a19c7579d8b23983ea2
Denny Fan <jmps820007@gmail.com>
2021-02-26 11:25:49 +0800

    Mine_Pratice

--- read.py
+++ read.py
@@ -1,14 +1,22 @@
 data = []
 count = 0
+lengths = 0
+avg = 0
+total = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        count += 1
-        if count % 1000 == 0:
-            print(len(data))
+        #count += 1
+        #if count % 1000 == 0:
+            #print(len(data))
+total = len(data)
+print('檔案讀取完了,總共有', len(data), '筆資料')
 
-print(len(data))
+with open('reviews.txt', 'r') as f:
+    for length in f:
+        lengths += len(length)
 
-print(data[0])
-print('---------------')
-print(data[1])
+avg = lengths / len(data)
+avg = int(avg)
+print('留言平均長度', avg)
+
