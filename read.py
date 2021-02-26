0e6166aaa0aa39c3e4907d93ca4a303769ee7992
Denny Fan <jmps820007@gmail.com>
2021-02-26 11:43:12 +0800

    My_Pratice2

--- read.py
+++ read.py
@@ -1,22 +1,31 @@
 data = []
 count = 0
-lengths = 0
-avg = 0
-total = 0
+
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        #count += 1
-        #if count % 1000 == 0:
-            #print(len(data))
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 total = len(data)
 print('檔案讀取完了,總共有', len(data), '筆資料')
 
-with open('reviews.txt', 'r') as f:
-    for length in f:
-        lengths += len(length)
 
-avg = lengths / len(data)
-avg = int(avg)
-print('留言平均長度', avg)
+print(len(data))     #印出data串列的--項目數量
+print(len(data[2]))  #印出data串列項目1的--字元數量
+print(data[2])
 
+sum_len = 0
+
+for d in data:
+    sum_len += len(d)
+
+print(sum_len)  #data串列的總字元數
+
+avg = 0
+
+avg = sum_len / count
+print(avg)
+#for d in data:
+#    sum_len += len(d)
+#print(sum_len)
