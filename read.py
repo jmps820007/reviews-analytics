17b0477c9b42d8cb46d22c20a23fcca95f92fc4f
Denny Fan <jmps820007@gmail.com>
2021-02-26 13:43:37 +0800

    add filter

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
-data = []
-count = 0
+data = []  ##用來裝reviews.txt的留言
+count = 0  ##計數目前檔案讀取到第幾筆
 
 with open('reviews.txt', 'r') as f:
     for line in f:
@@ -23,3 +23,10 @@ for d in data:
 print(sum_len)  #data串列的總字元數
 
 print("留言平均長度為", sum_len / count)
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '比留言長度小於100')
+print(new[0])
